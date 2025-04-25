f55844c1019d31ef1a14dab20524b342d005a871
jlima01 <jonatasflima01@gmail.com>
2025-04-25 00:09:27 -0300

    Edit!

--- Scripts/Player/PlayerInventory.cs
+++ Scripts/Player/PlayerInventory.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
-
+using TMPro;
 public class PlayerInventory : MonoBehaviour
 {
     public bool hasKey = false;
+    public float currentGold;
+    public TMP_Text goldText;
+    
+    public void AddGold(float amount)
+    {
+        currentGold += amount;
+
+        goldText.text = "" + currentGold;
+    }
 }
